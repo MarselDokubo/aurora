8406283306f57f037d3fdc85320f29f447711729
MarselDokubo <connectmarsel@gmail.com>
2024-08-22 07:49:59 +0100

    refactor(component+): category list

--- src/app/components/category-list.tsx
+++ src/app/components/category-list.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const CategoryList = async () => {
   return (
-    <div className="px-4 overflow-x-scroll scrollbar-hide">
+    <div className="px-4 overflow-x-scroll scrollbar-hidden">
       <div className="flex gap-4 md:gap-8">
         {[...new Array(15)].map((item, index) => (
           <Link
