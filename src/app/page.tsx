d22b9a70f767f8332d4681e6fb0f135d19fc51d3
MarselDokubo <connectmarsel@gmail.com>
2024-08-25 17:10:53 +0100

    refactor(page+): landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,18 @@
 import CategoryList from "@/app/components/category-list";
 import ProductList from "@/app/components/product-list";
 import Slider from "@/app/components/slider";
+import Landing from "./components/landing";
 
 const HomePage = async () => {
   return (
     <div className="">
-      <Slider />
-      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-        <h1 className="text-2xl">Featured Products</h1>
+      <Landing />
+      {/* <Slider /> */}
+      <div className="mt-44 px-4 md:px-8 lg:px-24">
+        <div className="heading flex">
+          <h1 className="text-6xl font-bold uppercase">For Him</h1>
+          <div className=""></div>
+        </div>
         <ProductList />
       </div>
       <div className="mt-24">
