8ed1569ba92a100350a85ed6bb845420128e61e2
MarselDokubo <connectmarsel@gmail.com>
2024-08-28 00:23:53 +0100

    build(component+): category list

--- src/app/components/category-list.tsx
+++ src/app/components/category-list.tsx
@@ -1,29 +1,42 @@
 import Image from "next/image";
 import Link from "next/link";
+import { wixClient } from "../lib/wixClient";
+
+const PRODUCT_PER_PAGE = 10;
 
 const CategoryList = async () => {
+  const cats = await wixClient.collections.queryCollections().find();
   return (
-    <div className="px-4 overflow-x-scroll scrollbar-hidden">
-      <div className="flex gap-4 md:gap-8">
-        {[...new Array(15)].map((item, index) => (
-          <Link
-            href={`/#`}
-            className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
-            key={index}
-          >
-            <div className="relative bg-slate-100 w-full h-96">
+    <div className="mt-6 flex gap-x-8 gap-y-16 justify-between overflow-scroll scrollbar-hidden">
+      {cats.items.map((item, index) => (
+        <Link
+          href={`/list?cat=${item.slug}`}
+          className="w-full flex flex-col gap-4 sm:w-[45%] lg:min-w-[33%] overflow-hidden"
+          key={item._id}
+        >
+          <div className="relative w-full aspect-[1/1.2]">
+            <Image
+              src={item.media?.mainMedia?.image?.url || "cat.png"}
+              alt=""
+              fill
+              sizes="25vw"
+              className="absolute object-cover z-10 hover:scale-[.98] transition-all easy duration-500"
+            />
+            {!item.media?.mainMedia?.image?.url && (
               <Image
-                src={"/cart.png"}
+                src={"/product.png"}
                 alt=""
                 fill
-                sizes="20vw"
-                className="object-cover"
+                sizes="25vw"
+                // className="absolute object-cover"
               />
-            </div>
-            <h1 className="mt-8 font-light text-xl tracking-wide">Item Name</h1>
-          </Link>
-        ))}
-      </div>
+            )}
+          </div>
+          <div className="flex justify-between">
+            <span className="font-semibold text-lg">Shop {item.name}</span>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 };
