cb45e4e1ed0b8628d375c80df4e731ce983acf2a
MarselDokubo <connectmarsel@gmail.com>
2024-08-28 00:24:47 +0100

    build(components+): product list

--- src/app/components/product-list.tsx
+++ src/app/components/product-list.tsx
@@ -1,41 +1,69 @@
 import Image from "next/image";
 import Link from "next/link";
+import { wixClient } from "../lib/wixClient";
+import { products } from "@wix/stores";
+import Pagination from "./pagination";
 
 const PRODUCT_PER_PAGE = 8;
-const ProductList = () => {
+const ProductList = async ({
+  categoryId,
+  limit,
+  searchParams,
+}: {
+  categoryId: string;
+  limit?: number;
+  searchParams?: any;
+}) => {
+  const res = await wixClient.products
+    .queryProducts()
+    .eq("collectionIds", categoryId ?? "")
+    .find();
+  console.log("ProductList", res);
+
   return (
-    <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
-      {[...new Array(PRODUCT_PER_PAGE)].map((product, index) => (
-        <Link
-          href={"/"}
-          className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-          key={index}
-        >
-          <div className="relative w-full h-80">
-            <Image
-              src={"/product.png"}
-              alt=""
-              fill
-              sizes="25vw"
-              className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
-            />
-            <Image
-              src={"/product.png"}
-              alt=""
-              fill
-              sizes="25vw"
-              className="absolute object-cover rounded-md"
-            />
-          </div>
-          <div className="flex justify-between">
-            <span className="font-medium">Product Name</span>
-            <span className="font-semibold">$45</span>
-          </div>
-          <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">
-            Add to Cart
-          </button>
-        </Link>
-      ))}
+    <div>
+      <div className="mt-6 flex gap-x-8 gap-y-16 justify-between overflow-scroll scrollbar-hidden">
+        {res.items.map((product: products.Product) => (
+          <Link
+            href={"/" + product.slug}
+            className="w-full flex flex-col gap-4 sm:w-[45%] lg:min-w-[33%]"
+            key={product._id}
+          >
+            <div className="relative w-full aspect-[1/1]">
+              <Image
+                src={product.media?.mainMedia?.image?.url || "/product.png"}
+                alt=""
+                fill
+                sizes="25vw"
+                className="absolute object-cover z-10 hover:opacity-0 transition-opacity easy duration-500"
+              />
+              {product.media?.items && (
+                <Image
+                  src={product.media?.items[1]?.image?.url || "/product.png"}
+                  alt=""
+                  fill
+                  sizes="25vw"
+                  className="absolute object-cover rounded-md"
+                />
+              )}
+            </div>
+            <div className="flex justify-between font-semibold text-lg">
+              <span className="">{product.name}</span>
+              <span className="">${product.price?.price}</span>
+            </div>
+            <button className="rounded-2xl ring-2 ring-black text-black w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">
+              Buy Now
+            </button>
+          </Link>
+        ))}
+      </div>
+      {searchParams?.cat || searchParams?.name ? (
+        <Pagination
+          currentPage={res.currentPage || 0}
+          hasPrev={res.hasPrev()}
+          hasNext={res.hasNext()}
+        />
+      ) : null}
     </div>
   );
 };
